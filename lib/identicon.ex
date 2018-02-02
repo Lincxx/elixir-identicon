4aaf5930f3524ee687d5ea58411b2062efdd0dfc
Jeremy Lincoln <bossco28@gmail.com>
2018-02-02 10:15:36 -0500

    Add in row mirroring, building a grid

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -4,17 +4,28 @@ defmodule Identicon do
     input
     |> hash_input
     |> pick_color
+    |> build_grid
   end
 
-  def pick_color(image) do
-   %Identicon.Image{hex: hex_list} = image
-   #| _tail says I ackonwledge that there are others in the list, but I don't care about them
-   [r, g, b | _tail] = hex_list
-   [r,g,b]
-
-  #  or we can shrink this 
-  #   %Identicon.Image{hex: [r, g, b | _tail]} = image
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    hex
+    |> Enum.chunk(3)
+    |> Enum.map(&mirror_row/1)
   end
+
+  def mirror_row(row) do
+    # [145, 46, 200]
+    [first, second | _tail] = row
+
+    #[145, 46, 200, 46, 145]
+    row ++ [second, first]
+  end
+
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+   %Identicon.Image{image | color: {r, g, b}}
+  end
+
+  
 
   def hash_input(input) do
     hex = :crypto.hash(:md5, input)
